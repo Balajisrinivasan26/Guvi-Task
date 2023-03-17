eda461798707bb8605f3c797d24727864305b59f
balaji <balajis@gmail.com>
2023-03-17 14:38:37 +0530

    redis

--- php/login.php
+++ php/login.php
@@ -9,6 +9,12 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379);
+
+ini_set('session.save_handler', 'redis');
+ini_set('session.save_path', 'tcp://127.0.0.1:6379');
+
 $email = $_POST["email"];
 $password = $_POST["password"];
 
@@ -23,6 +29,10 @@ if (mysqli_num_rows($result) == 0) {
     $row = mysqli_fetch_assoc($result);
     
     if($password== $row['password']){
+        $session_id = uniqid();
+        $redis->set("session:$session_id", $email);
+        $redis->expire("session:$session_id", 10*60);
+       
         $payload = array(
             "email" => $row['email'],
             "expires_at" => time() + 3600 // Expires in 1 hour
